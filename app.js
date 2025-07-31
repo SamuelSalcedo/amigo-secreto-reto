1268095ff914200b9d929b5d4ed77b746df236cb
Samuel Salcedo <salcedolucerosamuel2@gmail.com>
2025-07-30 21:48:18 -0600

    validando el input

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// //Aquí deberás desarrollar la lógica para resolver el problema.
+
+///Agregar el nombre que los usuarios ingresan haciendo clic en el boton adicionar
+
+//Valida que el input no este vacio si lo esta el sistema manda una alerta
+
+//Ver la lista de los nombres ingresados en el campo de entrada
+
+//Al hacer clic en el boton de sortear se seleccionara un nombe de la lista y se mostrara en la pagina
+// id de lista: listaAmigos
+// id:" resultado
+//Funcion que valida el input
+let listadoAmigos = [];
+let listado = document.querySelector('listaAmigos');
+let input ="";
+
+function validarInput(){
+    input = document.getElementById('amigo').value;
+
+    if(input == ""){
+        alert("Por favor, inserte un nombre.");
+        return false
+    }else{
+        console.log(input);
+        return true
+    }
+}
+
+function limpiarInput(){
+    document.querySelector('#amigo').value = "";
+}
+function agregarAmigo(){
+    validarInput();
+}
+
+function sortearAmigo(){
+
+}
