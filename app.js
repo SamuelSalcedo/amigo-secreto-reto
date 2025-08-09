08df89e57df4f7457b32bab79c4a0f750cabe701
Samuel Salcedo <salcedolucerosamuel2@gmail.com>
2025-08-08 22:41:06 -0600

    Agregando parejas

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ let listadoAmigos = [];
 let input ="";
 let listadoHTML = document.getElementById('listaAmigos');
 let respuestaHTML = document.getElementById('resultado');
-
+let botonParejas = document.getElementById("button-couples");
+let botonAdd = document.getElementById('button-add');
+let botonsorteo = document.getElementById('button-sort');
 function validarInput(){
     input = document.getElementById('amigo').value;
     if(input === ""){
@@ -60,13 +62,78 @@ function sortearAmigo(){
         mostrarAlerta('La lista esta vacia ingrese un nombre');
     }else{
         //sortear algun amigo aleatorio
-        let numAmigo = Math.floor(Math.random()*listadoAmigos.length-1)+1;
+        let numAmigo = sortearAleatorio();
         respuestaHTML.innerHTML = `<li> ${listadoAmigos[numAmigo]}</li>`
         console.log(numAmigo);
         console.log(listadoAmigos[numAmigo]);
     }
 }
 
+function formarParejas(){
+    respuestaHTML.innerHTML = ""
+
+    if(listadoAmigos.length === 0){
+        mostrarAlerta('La lista esta vacia ingrese un nombre');
+
+    }else{
+        //formar parejas 
+        //Ver si el arreglo es par o impar si es par se deja
+        //si es impar se hace push al arreglo 'sin pareja'
+        //tomar numeros aleatorios del arreglo que no se repiten:
+            //si  el num2 es igual del num1 saca otro 
+            //agrega los elementos a un nueov arreglo para verficar que se sorteen todos
+            //agrega al listado y num 1 y num 2 se vacian
+            
+            //haz esto while hasta que el nuevo arreglo sea igual al inicial
+        if(listadoAmigos.length%2 != 0){
+            //es par no se hace nada
+            listadoAmigos.push('Sin pareja');
+        }    
+        let nuevaLista = [];
+        let numAmigo = 0;
+        let numAmigo2= 0;
+
+        while(nuevaLista.length != listadoAmigos.length){
+            
+            //mientras que sea el mismo saca otro
+            while(numAmigo2 == numAmigo || (nuevaLista.includes(numAmigo) || (nuevaLista.includes(numAmigo2)))){
+                numAmigo2 = sortearAleatorio();
+                //tomar un numero aleatorio
+                numAmigo = sortearAleatorio();
+                
+            }
+
+            //console.log(numAmigo);
+            //console.log(numAmigo2);
+            
+            nuevaLista.push(numAmigo,numAmigo2);
+
+            respuestaHTML.innerHTML += `<li> ${listadoAmigos[numAmigo]} - ${listadoAmigos[numAmigo2]} </li>`
+           // console.log("Nueva lista: " + nuevaLista);
+            numAmigo = 0;
+            numAmigo2 = 0;
+        }
+        botonParejas.disabled = true;
+        botonAdd.disabled = true;        
+        botonsorteo.disabled = true;
+    }
+}
+
+function sortearAleatorio(){
+    return Math.floor(Math.random()*listadoAmigos.length-1)+1;
+}
+
+
+function reiniciarJuego(){
+    limpiarInput();
+    botonParejas.disabled = false;
+    botonAdd.disabled = false;
+    botonsorteo.disabled = false;
+    listadoAmigos = [];
+    respuestaHTML.innerHTML = "";
+    listadoHTML.innerHTML = "";
+    console.log(listadoAmigos)
+}
 
 //Funcion para mostrar la alerta personalizada
 function mostrarAlerta(mensaje){
